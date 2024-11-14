35c387de2c06a9ec3e82e8b4c119619d1203c19b
fco-junior <cassemirojunior16@gmail.com>
2024-11-13 22:59:52 -0300

    Implementing routing with react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { RouterProvider } from 'react-router-dom';
+import { Sidebar } from './components/Sidebar';
+import {route} from './routes';
 import './global.css';
 
-import { Sidebar } from './components/Sidebar';
-import { Tweet } from './components/Tweet';
-import { Header } from './components/Header';
-import { Separator } from './components/Separator';
-
-import tweets from './data/tweetsMock';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,37 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Sidebar />
 
       <div className="content">
-        <main className="timeline">
-          <Header title="Home" />
-
-          <form className="new-tweet-form">
-            <label htmlFor="tweet">
-              <img src="https://github.com/fco-junior.png" alt="Fco Costa Cassemiro Jr" />
-              <textarea id="tweet" placeholder="What's happening?" />
-            </label>
-
-            <button type="submit">Tweet</button>
-          </form>
-
-          <Separator />
-
-          {
-            tweets.map(tweet => {
-              return (
-                <Tweet
-                  key={tweet.user}
-                  icon={tweet.icon}
-                  name={tweet.name}
-                  user={tweet.user}
-                  content={tweet.content}
-                  likes={tweet.likes}
-                  retweets={tweet.retweets}
-                  comments={tweet.comments}
-                />
-              )
-            })
-          }
-        </main>
+        <RouterProvider router={route} />
       </div>
     </div>
   </StrictMode>,
---
+++ src/pages/Timeline.tsx
@@ -0,0 +1,43 @@
+import { Header } from "../components/Header"
+import { Separator } from "../components/Separator"
+import { Tweet } from "../components/Tweet"
+
+import tweets from '../data/tweetsMock';
+
+import './Timeline.css';
+
+export function Timeline() {
+  return (
+    <main className="timeline">
+      <Header title="Home" />
+
+      <form className="new-tweet-form">
+        <label htmlFor="tweet">
+          <img src="https://github.com/fco-junior.png" alt="Fco Costa Cassemiro Jr" />
+          <textarea id="tweet" placeholder="What's happening?" />
+        </label>
+
+        <button type="submit">Tweet</button>
+      </form>
+
+      <Separator />
+
+      {
+        tweets.map(tweet => {
+          return (
+            <Tweet
+              key={tweet.user}
+              icon={tweet.icon}
+              name={tweet.name}
+              user={tweet.user}
+              content={tweet.content}
+              likes={tweet.likes}
+              retweets={tweet.retweets}
+              comments={tweet.comments}
+            />
+          )
+        })
+      }
+    </main>
+  )
+}
